7a5f9623f5140b27263147db5d64f82f91b7194f
郭泰竹 <102306037@nccu.edu.tw>
2017-06-13 01:12:06 +0800

    commit

--- server.R
+++ server.R
@@ -10,77 +10,167 @@ library(grid) #for rendering a raster grob
 
 
 shinyServer(function(input, output) {
-  
+  final <- reactive({
+    
+    if(input$goButton){
+      if(input$radio =="1"){
+        having = as.integer(input$target)-as.integer(input$savings)*(1+(as.integer(input$savingratio)*0.01))^(as.integer(input$period))
+        invest_ratio = c()
+        if(having>0){
+          for( i in 1:(as.integer(input$period)-1)){
+            invest_ratio[i] = (as.integer(input$tool1)*0.01)*(1+(as.integer(input$ratio1)*0.01))^i+(as.integer(input$tool2)*0.01)*(1+(as.integer(input$ratio2)*0.01))^i+(as.integer(input$tool3)*0.01)*(1+(as.integer(input$ratio3)*0.01))^i
+          }
+          final = having/sum(invest_ratio)
+          final 
+        }else{
+          final = -1
+          final 
+        }
+      }else{
+        having = as.integer(input$target)-as.integer(input$savings)*(1+(as.integer(input$savingratio)*0.01))^(as.integer(input$period))
+        invest_ratio = c()
+        if(having>0){
+          for( i in 1:(as.integer(input$period)*12-1)){
+            invest_ratio[i] = (as.integer(input$tool1)*0.01)*(1+(as.integer(input$ratio1)*0.01/12))^i+(as.integer(input$tool2)*0.01)*(1+(as.integer(input$ratio2)*0.01/12))^i+(as.integer(input$tool3)*0.01)*(1+(as.integer(input$ratio3)*0.01/12))^i
+          }
+          final = having/sum(invest_ratio)
+          final 
+        }else{
+          final = -1
+          final 
+        }
+        
+      }
+    }
+  })
   output$text1 <- renderText({ 
-    input$goButton
-    #年計:
-    #A-B*(1+r)^n
-    #=C*d%*(1+r1)^n+C*e%*(1+r2)^n+C*f%*(1+r3)^n == 投資部分總利合
-    if(input$radio =="1"){
-      having = as.integer(input$target)-as.integer(input$savings)*(1+(as.integer(input$savingratio)*0.01))^(as.integer(input$period))
-      invest_ratio = c()
-      for( i in 1:(as.integer(input$period)-1)){
-        invest_ratio[i] = (as.integer(input$tool1)*0.01)*(1+(as.integer(input$ratio1)*0.01))^i+(as.integer(input$tool2)*0.01)*(1+(as.integer(input$ratio2)*0.01))^i+(as.integer(input$tool3)*0.01)*(1+(as.integer(input$ratio3)*0.01))^i
-      }
-      final = having/sum(invest_ratio)
-      paste0("以年計息，你平均需要投資",final,"元/年") 
+    
+    
+    if(is.null(final())){
+      paste0("Welcome to the Saving & Investment Helper!") 
+    }else if (final()==-1){
+      paste0("Saving is enough!") 
+    } else if(input$radio =="1"){
+      # having = as.integer(input$target)-as.integer(input$savings)*(1+(as.integer(input$savingratio)*0.01))^(as.integer(input$period))
+      # invest_ratio = c()
+      # for( i in 1:(as.integer(input$period)-1)){
+      #   invest_ratio[i] = (as.integer(input$tool1)*0.01)*(1+(as.integer(input$ratio1)*0.01))^i+(as.integer(input$tool2)*0.01)*(1+(as.integer(input$ratio2)*0.01))^i+(as.integer(input$tool3)*0.01)*(1+(as.integer(input$ratio3)*0.01))^i
+      # }
+      # final = having/sum(invest_ratio)
+      paste0("By year, you need to invest : ",as.integer(final()),"  NTD/year") 
     }else{
-      having = as.integer(input$target)-as.integer(input$savings)*(1+(as.integer(input$savingratio)*0.01))^(as.integer(input$period))
-      invest_ratio = c()
-      for( i in 1:(as.integer(input$period)*12-1)){
-        invest_ratio[i] = (as.integer(input$tool1)*0.01)*(1+(as.integer(input$ratio1)*0.01/12))^i+(as.integer(input$tool2)*0.01)*(1+(as.integer(input$ratio2)*0.01/12))^i+(as.integer(input$tool3)*0.01)*(1+(as.integer(input$ratio3)*0.01/12))^i
-      }
-      final = having/sum(invest_ratio)
-      paste0("以月計息，你平均需要投資",final,"元/月") 
+      # having = as.integer(input$target)-as.integer(input$savings)*(1+(as.integer(input$savingratio)*0.01))^(as.integer(input$period))
+      # invest_ratio = c()
+      # for( i in 1:(as.integer(input$period)*12-1)){
+      #   invest_ratio[i] = (as.integer(input$tool1)*0.01)*(1+(as.integer(input$ratio1)*0.01/12))^i+(as.integer(input$tool2)*0.01)*(1+(as.integer(input$ratio2)*0.01/12))^i+(as.integer(input$tool3)*0.01)*(1+(as.integer(input$ratio3)*0.01/12))^i
+      # }
+      # final = having/sum(invest_ratio)
+      paste0("By month, you need to invest : ",as.integer(final()),"  NTD/month") 
     }
     
     
   })
-  
+  peryear_new <- reactive({
+    
+    if(input$goButton){
+      if(input$radio =="1"){
+        having = as.integer(input$target)-as.integer(input$savings)*(1+(as.integer(input$savingratio)*0.01))^(as.integer(input$period))
+        invest_ratio = c()
+        if(having >0){
+          for( i in 1:(as.integer(input$period)-1)){
+            invest_ratio[i] = (as.integer(input$tool1)*0.01)*(1+(as.integer(input$ratio1)*0.01))^i+(as.integer(input$tool2)*0.01)*(1+(as.integer(input$ratio2)*0.01))^i+(as.integer(input$tool3)*0.01)*(1+(as.integer(input$ratio3)*0.01))^i
+          }
+          final = having/sum(invest_ratio)
+          peryear = final*invest_ratio
+          #plot(peryear)
+          peryear_new = c()
+          for(i in 1:(as.integer(input$period)-1)){
+            if(i==1){
+              peryear_new[1] = peryear[1]
+            }else{
+              peryear_new[i] = sum(peryear[1:i])
+            }
+          }
+          peryear_new
+        }else{
+          peryear_new = -1
+          peryear_new
+        }
+        
+      }else{
+        having = as.integer(input$target)-as.integer(input$savings)*(1+(as.integer(input$savingratio)*0.01))^(as.integer(input$period))
+        invest_ratio = c()
+        if(having >0){
+          for( i in 1:(as.integer(input$period)*12-1)){
+            invest_ratio[i] = (as.integer(input$tool1)*0.01)*(1+(as.integer(input$ratio1)*0.01/12))^i+(as.integer(input$tool2)*0.01)*(1+(as.integer(input$ratio2)*0.01/12))^i+(as.integer(input$tool3)*0.01)*(1+(as.integer(input$ratio3)*0.01/12))^i
+          }
+          final = having/sum(invest_ratio)
+          peryear = final*invest_ratio
+          #
+          peryear_new = c()
+          for(i in 1:(as.integer(input$period)*12-1)){
+            if(i==1){
+              peryear_new[1] = peryear[1]
+            }else{
+              peryear_new[i] = sum(peryear[1:i])
+            }
+          }
+          peryear_new
+        }else{
+          peryear_new=-1
+          peryear_new
+        }
+        
+      }
+    }
+  })
   output$plot1 <- renderPlot({
-    input$action
-    if(input$radio =="1"){
-      having = as.integer(input$target)-as.integer(input$savings)*(1+(as.integer(input$savingratio)*0.01))^(as.integer(input$period))
-      invest_ratio = c()
-      for( i in 1:(as.integer(input$period)-1)){
-        invest_ratio[i] = (as.integer(input$tool1)*0.01)*(1+(as.integer(input$ratio1)*0.01))^i+(as.integer(input$tool2)*0.01)*(1+(as.integer(input$ratio2)*0.01))^i+(as.integer(input$tool3)*0.01)*(1+(as.integer(input$ratio3)*0.01))^i
-      }
-      final = having/sum(invest_ratio)
-      peryear = final*invest_ratio
-      #plot(peryear)
-      peryear_new = c()
-      for(i in 1:(as.integer(input$period)-1)){
-        if(i==1){
-          peryear_new[1] = peryear[1]
-        }else{
-          peryear_new[i] = sum(peryear[1:i])
-        }
-      }
+    if(is.null(peryear_new())){
+      
+    }else if (peryear_new()==-1){
+      paste0("Saving is enough!") 
+    }else if(input$radio =="1"){
+      # having = as.integer(input$target)-as.integer(input$savings)*(1+(as.integer(input$savingratio)*0.01))^(as.integer(input$period))
+      # invest_ratio = c()
+      # for( i in 1:(as.integer(input$period)-1)){
+      #   invest_ratio[i] = (as.integer(input$tool1)*0.01)*(1+(as.integer(input$ratio1)*0.01))^i+(as.integer(input$tool2)*0.01)*(1+(as.integer(input$ratio2)*0.01))^i+(as.integer(input$tool3)*0.01)*(1+(as.integer(input$ratio3)*0.01))^i
+      # }
+      # final = having/sum(invest_ratio)
+      # peryear = final*invest_ratio
+      # plot(peryear)
+      # peryear_new = c()
+      # for(i in 1:(as.integer(input$period)-1)){
+      #   if(i==1){
+      #     peryear_new[1] = peryear[1]
+      #   }else{
+      #     peryear_new[i] = sum(peryear[1:i])
+      #   }
+      # }
       #draw
-      plot(peryear_new,pch=20,lwd=5,las=1,ann = FALSE)
-      lines(peryear_new,col="red",lwd=2)
+      plot(peryear_new(),pch=20,lwd=5,las=1,ann = FALSE)
+      lines(peryear_new(),col="red",lwd=2)
     }else{
-      having = as.integer(input$target)-as.integer(input$savings)*(1+(as.integer(input$savingratio)*0.01))^(as.integer(input$period))
-      invest_ratio = c()
-      for( i in 1:(as.integer(input$period)*12-1)){
-        invest_ratio[i] = (as.integer(input$tool1)*0.01)*(1+(as.integer(input$ratio1)*0.01/12))^i+(as.integer(input$tool2)*0.01)*(1+(as.integer(input$ratio2)*0.01/12))^i+(as.integer(input$tool3)*0.01)*(1+(as.integer(input$ratio3)*0.01/12))^i
-      }
-      final = having/sum(invest_ratio)
-      peryear = final*invest_ratio
-      #
-      peryear_new = c()
-      for(i in 1:(as.integer(input$period)*12-1)){
-        if(i==1){
-          peryear_new[1] = peryear[1]
-        }else{
-          peryear_new[i] = sum(peryear[1:i])
-        }
-      }
+      # having = as.integer(input$target)-as.integer(input$savings)*(1+(as.integer(input$savingratio)*0.01))^(as.integer(input$period))
+      # invest_ratio = c()
+      # for( i in 1:(as.integer(input$period)*12-1)){
+      #   invest_ratio[i] = (as.integer(input$tool1)*0.01)*(1+(as.integer(input$ratio1)*0.01/12))^i+(as.integer(input$tool2)*0.01)*(1+(as.integer(input$ratio2)*0.01/12))^i+(as.integer(input$tool3)*0.01)*(1+(as.integer(input$ratio3)*0.01/12))^i
+      # }
+      # final = having/sum(invest_ratio)
+      # peryear = final*invest_ratio
+      # #
+      # peryear_new = c()
+      # for(i in 1:(as.integer(input$period)*12-1)){
+      #   if(i==1){
+      #     peryear_new[1] = peryear[1]
+      #   }else{
+      #     peryear_new[i] = sum(peryear[1:i])
+      #   }
+      # }
       #draw
-      plot(peryear_new,pch=20,lwd=5,las=1,ann = FALSE)
-      lines(peryear_new,col="red",lwd=2)
-      print(final)
-      print(peryear_new)
+      plot(peryear_new(),pch=20,lwd=5,las=1,ann = FALSE)
+      lines(peryear_new(),col="red",lwd=2)
+      #print(final)
+      #print(peryear_new)
     }
   })
   output$plot2 <- renderPlot({
--- ui.R
+++ ui.R
@@ -2,29 +2,27 @@ shinyUI(fluidPage(
   titlePanel("Saving cacultor"),
   sidebarLayout(
     sidebarPanel(
-      textInput("target", "目標儲蓄金額(元):", 0,width=300),
-      div(style="display:inline-block",textInput("savings", "目前存款金額(元):", 0)),
-      div(style="display:inline-block",textInput("savingratio", "存款年利率(%):", 0)),
+      textInput("target", "Target(NTD):", 0,width=300),
+      div(style="display:inline-block",textInput("savings", "Savings(NTD):", 0,width=200)),
+      div(style="display:inline-block",textInput("savingratio", "Saving ratio(%):", 0,width=200)),
       div(),
-      div(style="display:inline-block",textInput("tool1", "第一投資工具比例(%):", value = 0)),
-      div(style="display:inline-block",textInput("tool2", "第二投資工具比例(%):", value = 0)),
-      div(style="display:inline-block",textInput("tool3", "第三投資工具比例(%):", value = 0)),
+      div(style="display:inline-block",textInput("tool1", "Tool 1(%):", value = 0,width=120)),
+      div(style="display:inline-block",textInput("tool2", "Tool 2(%):", value = 0,width=120)),
+      div(style="display:inline-block",textInput("tool3", "Tool 3(%):", value = 0,width=120)),
       
-      div(style="display:inline-block",textInput("ratio1", "第一投資報酬率/年(%):", value = 0)),
-      div(style="display:inline-block",textInput("ratio2", "第二投資報酬率/年(%):", value = 0)),
-      div(style="display:inline-block",textInput("ratio3", "第三投資報酬率/年(%):", value = 0)),
+      div(style="display:inline-block",textInput("ratio1", "ratio 1/year(%):", value = 0,width=120)),
+      div(style="display:inline-block",textInput("ratio2", "ratio 2/year(%):", value = 0,width=120)),
+      div(style="display:inline-block",textInput("ratio3", "ratio 3/year(%):", value = 0,width=120)),
       
-      #textInput("loan", "預計貸款金額(元):", 0,width=200),
-      #textInput("loanrate", "預計貸款利率(%):", 0,width=200),
-      textInput("period", "預估儲蓄時間(年):", 0,width=200),
-      radioButtons("radio", label = "複利次數", choices = list("每年" = 1, "每月" = 2)),
+      textInput("period", "saving period(year):", 0,width=200),
+      radioButtons("radio", label = "Compound interest by", choices = list("Every Year" = 1, "Every Month" = 2)),
       actionButton("goButton", label = "Go!",width=150),
       
-      h4('說明:'),
+      h4('P.S.'),
       
-      p('1. 請依照上列要求填入對照金額'),
+      p('1. Enter the correspond number above'),
       
-      p('2. 按下Go!則輸出報表')
+      p('2. press GO! and output the report')
       
       
     
